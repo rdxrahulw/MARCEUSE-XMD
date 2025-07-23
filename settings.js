ba35de1f9e1db861549374ab8ff52b8a738f9ee6
Nicolaus-Daniel <nicolausd9@gmail.com>
2025-07-23 21:04:57 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'MARCEUSE-XMD😈',
+  author: '‎',
+  botName: "MARCEUSE-XMD😈",
+  botOwner: 'Nicolaus Daniel 2😈😈😈', // Your name
+  ownerNumber: '255652398614', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.6",
+};
+
+module.exports = settings;

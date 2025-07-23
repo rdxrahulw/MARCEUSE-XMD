55a2aa6bab6a9dc2689672e1ac31187fd5b09308
Nicolaus-Daniel <nicolausd9@gmail.com>
2025-07-23 21:27:26 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "MARCEUSE-XMD😈",
   botOwner: 'Nicolaus Daniel 2😈😈😈', // Your name
-  ownerNumber: '255652398614', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '2556523xxxx', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
